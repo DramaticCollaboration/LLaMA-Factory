4afea16121759a4b4b99c9655f4411e726923c20
poh <qusansdl@gmail.com>
2025-10-29 16:12:50 +0900

    set host

--- docker/empasy/docker-cuda/ebook-to-mindmap/vite.config.ts
+++ docker/empasy/docker-cuda/ebook-to-mindmap/vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
     server: {
-        host: true,
+        host: '0.0.0.0',
         // 또는 '0.0.0.0'으로 지정할 수 있습니다.
         // host: '0.0.0.0'
     },
