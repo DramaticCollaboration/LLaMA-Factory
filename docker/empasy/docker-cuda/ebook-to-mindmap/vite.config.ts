4cd08e9e4a254713505c50a9666d3d7a64219376
poh <qusansdl@gmail.com>
2025-10-29 16:11:23 +0900

    lib 추가

--- docker/empasy/docker-cuda/ebook-to-mindmap/vite.config.ts
+++ docker/empasy/docker-cuda/ebook-to-mindmap/vite.config.ts
@@ -5,10 +5,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+    server: {
+        host: true,
+        // 또는 '0.0.0.0'으로 지정할 수 있습니다.
+        // host: '0.0.0.0'
+    },
+    plugins: [react(), tailwindcss()],
     resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
+    },
 })
